47c53303a4a7ca7b39816d5fe8728eeabedc0905
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-09-14 12:14:30 -0300

    Update block_data.py

--- block_data.py
+++ block_data.py
@@ -1,18 +1,15 @@
-from hexbytes import HexBytes
-from web3 import Web3
+from web3 import Web3                            # Import the web3 library at the top.
+from hexbytes import HexBytes                    # Import the hexbytes to convert bytes into HEX.
 
-# Node endpoint
-node_url = "CHAINSTACK_NODE_URL"
+node_url = input('Insert your Node URL: ')       # Accepts the user node URL.
+web3 = Web3(Web3.HTTPProvider(node_url))         # Establish connection to the node.
 
-# Create the node connection
-web3 = Web3(Web3.HTTPProvider(node_url))
-
-# Verify if the connection is successful
-if web3.isConnected():
-    print("Connection Successful")
+# Verify if the connection is successful. This is optional, but it's nice to notify the user.
+if web3.isConnected():                                                          
+    print('Connection Successful')
     print('-' * 50)
 else:
-    print("Connection Failed")
+    print('Connection Failed')
 
 # Identify and print the latest block number
 last = web3.eth.block_number
@@ -22,8 +19,8 @@ print('Block information:')
 print('-' * 50)
 
 # Retrieve the block data from the blockchain
-block = web3.eth.get_block('latest')
-#print(block) # this would print the raw data
+block = web3.eth.get_block('latest')                # You can also specify the block as a block number, or pending, latest, and earliest.
+#print(block)                                       # this would print the raw data
 
 # convert the web3 object retrieved into a Python dictionary
 dictionary = dict(block)
